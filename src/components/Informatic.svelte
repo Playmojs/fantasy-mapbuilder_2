23e4f2d7e063ccae57d578a5972fe77d23e9b5c6
AlexW <alex.wulfsberg98@gmail.com>
2024-07-14 20:46:15 +0200

    Add Toolbar, changed infromatic hight

--- src/components/Informatic.svelte
+++ src/components/Informatic.svelte
@@ -28,27 +28,20 @@
 			resizer.addEventListener('mousedown', (e) => {
 				e.preventDefault();
 				windowWidth = window.innerWidth / 100;
-				windowHeight = window.innerHeight / 100;
 
 				originalX = informaticWindow.getBoundingClientRect().left / windowWidth;
-				originalY = informaticWindow.getBoundingClientRect().top / windowHeight;
 				originalMouseX = e.pageX;
-				originalMouseY = e.pageY;
 				window.addEventListener('mousemove', resize);
 				window.addEventListener('mouseup', stopResize);
 			});
 
 			function resize(e: MouseEvent) {
 				let newX = originalX + (e.pageX - originalMouseX) / windowWidth;
-				let newY = originalY + (e.pageY - originalMouseY) / windowHeight;
 
-				newY = newY > 1 ? newY : 0;
 				newX = newX < 92 ? newX : 92;
 
 				informaticWindow.style.left = `${newX}%`;
-				informaticWindow.style.top = `${newY}%`;
 				informaticWindow.style.width = `${100 - newX}%`;
-				informaticWindow.style.height = `${100 - newY}%`;
 			}
 
 			function stopResize() {
@@ -108,20 +101,21 @@
 	#informaticWindow {
 		position: fixed;
 		background-color: rgb(47, 47, 47);
-		top: 25%;
+		top: 0%;
 		left: 66%;
-		height: 75%;
+		height: 100%;
 		width: 34%;
 		z-index: 10;
 	}
 
 	#informatic {
 		position: relative;
-		height: 85%;
+		top: 30%;
+		height: 60%;
 		left: 0;
 		right: 0;
 		background-color: inherit;
-		font-size: 100px;
+		font-size: large;
 		text-align: justify;
 		overflow-y: scroll;
 		padding: 10px;
@@ -155,7 +149,7 @@
 
 	#toolbar {
 		position: relative;
-		height: 10%;
+		height: 5%;
 		width: 100%;
 		display: flex;
 		align-items: center;
@@ -165,8 +159,9 @@
 	#edit_content_button {
 		position: relative;
 		background-color: #555;
-		height: 60%;
+		height: 100%;
 		margin-right: 10px;
+		margin-top: 10px;
 		shape-outside: circle();
 		border-radius: 7%;
 	}
@@ -183,9 +178,9 @@
 		position: absolute;
 		top: 0;
 		left: 0;
-		cursor: nwse-resize;
-		width: 100px;
-		height: 100px;
+		cursor: e-resize;
+		width: 10px;
+		height: 100%;
 		z-index: 10;
 	}
 </style>
