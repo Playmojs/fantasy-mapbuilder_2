61dcd9e666d40a7a8eca8ec40d9ec9d4066d6cb1
AlexW <alex.wulfsberg98@gmail.com>
2024-07-26 19:03:26 +0200

    cleanup page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,6 @@
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	
-
-	let markers = [
-		{ position: { x: 30, y: 50 }, image: '/assets/marker.png', map_id: 'map1' },
-		{ position: { x: 60, y: 20 }, image: '/assets/marker.png', map_id: 'map2' }
-	];
-
 	onMount(() => {
     	goto('/1');
   	});
