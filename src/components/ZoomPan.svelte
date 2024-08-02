643d64d99e8687fabd8d86612493851de0665103
Simen <simen.st@hotmail.com>
2024-08-02 09:58:25 +0200

    Correct zoom system for touch screen

--- src/components/ZoomPan.svelte
+++ src/components/ZoomPan.svelte
@@ -52,17 +52,19 @@
 	}
 
 	function handle_touch_move(event: TouchEvent) {
+		event.preventDefault()
 		if (event.touches.length === 1 && is_panning) {
 			current_x = clamp(event.touches[0].clientX - start_x, get_min_x(), 0);
 			current_y = clamp(event.touches[0].clientY - start_y, get_min_y(), get_max_y());
 		} else if (event.touches.length === 2) {
+			is_panning = false;
 			const new_distance = get_distance(event.touches[0], event.touches[1]);
-			scale = clamp(initial_scale * (new_distance / initial_distance), min_scale, max_scale);
+			let new_scale = clamp(initial_scale * (new_distance / initial_distance), min_scale, max_scale);
 
 			const center_x = (event.touches[0].clientX + event.touches[1].clientX) / 2;
 			const center_y = (event.touches[0].clientY + event.touches[1].clientY) / 2;
 
-			zoom_at(center_x, center_y, scale);
+			zoom_at(center_x, center_y, new_scale);
 		}
 		update_transform();
 	}
