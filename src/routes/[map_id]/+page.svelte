83a2bffb58096c2d815c02c7c4a7df8e7990bf95
Simen Storesund <simen.st@hotmail.com>
2024-08-26 10:45:22 +0200

    Clean up navigation pages

--- src/routes/[map_id]/+page.svelte
+++ src/routes/[map_id]/+page.svelte
@@ -5,6 +5,30 @@
 	import Toolbar from '../../components/Toolbar.svelte';
 	import ParentMap from '../../components/ParentMap.svelte';
 	import Modal from '../../components/Modal.svelte';
+
+	import { page } from '$app/stores';
+	import dtb from '$lib/dtb';
+	import { onDestroy } from 'svelte';
+
+	let unsubscribe = page.subscribe(async (value) => {
+		const map_id = +value.params.map_id;
+		const map = await dtb.get_map(map_id);
+		if (map) {
+			store.map = map;
+		}
+		const article = await dtb.get_article(store.map.article_id);
+		if (article) {
+			store.article = article;
+		}
+		const current_markers = await dtb.get_markers(store.map.id);
+		if (current_markers) {
+			store.markers = current_markers;
+		}
+	});
+
+	onDestroy(() => {
+		unsubscribe();
+	});
 </script>
 
 <Map />
