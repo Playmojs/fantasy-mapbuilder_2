5e154aa7fe2cf6e06c54fdaf8b91c34c9f2b210c
Playmojs <simen.st@hotmail.com>
2025-01-11 18:29:29 +0100

    Revert "Remove alex test to check if this fixes deployment problems"
    This reverts commit 371f2d8d877e500a56cc602e49e772a2cd6f863c.

---
+++ src/routes/alex_test/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Toolbar from '../../components/Toolbar.svelte';
+</script>
+
+<Toolbar />
+
+<style>
+	body {
+		margin: 0;
+		font-family: Arial, sans-serif;
+	}
+
+	.content {
+		margin-top: 60px; /* Adjust based on toolbar height */
+		padding: 20px;
+	}
+</style>
