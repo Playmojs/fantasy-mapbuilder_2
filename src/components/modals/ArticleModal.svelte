ca8c4569ff1a0fbf355c4ad1c078c9ccc4a30909
Playmojs <simen.st@hotmail.com>
2025-01-11 18:31:33 +0100

    Revert "Improve Article Modal design and add edit functionality"
    This reverts commit 64dab68f5cf8bce75f0bbe1640ffc4c1371bbc75.

--- src/components/modals/ArticleModal.svelte
+++ src/components/modals/ArticleModal.svelte
@@ -3,41 +3,33 @@
 	import { store } from '../../store.svelte';
 	import Editor from '../Editor.svelte';
 	import dtb from '$lib/dtb';
-	import { add_and_choose_article, choose_article_by_id, get_article_options, get_composite_category_modal, get_filtered_choose_category_modal, push_modal, push_promise_modal } from '$lib/modal_manager.svelte';
+	import { choose_article_by_id, get_article_options, push_modal, push_promise_modal } from '$lib/modal_manager.svelte';
 	import KeyWordRenderer from '../KeyWordRenderer.svelte';
-	import { onDestroy, onMount } from 'svelte';
-	import type { Category } from '$lib/types';
-	import { theme_entities } from '$lib/data.svelte';
+	import { pop_article, undo_article_pop} from '$lib/article_stack';
+	import type { Article } from '$lib/types';
+	import { onMount } from 'svelte';
 
-	let {close, article_id, on_close} : {close: () => void, article_id: number, on_close?: (success: boolean) => void} = $props()
-	
 	let article_title: HTMLHeadElement;
-	let editor = $state<Editor>();
 
+    let {close, article_id, on_close} : {close: () => void, article_id: number, on_close?: (success: boolean) => void} = $props()
 
 	let article = $derived({...store.article_cache[article_id]})
-	let latest_editor_content = $state<string>('')
-
 	let text_size = $state<number>(150)
-
-	let edit_mode = $state<boolean>(false)
 
 	let on_display = $state<'front-page' | 'main'>('front-page')
 
-	let current_categories = $derived<Category[]>(Object.entries(store.article_category_links).filter(([category_id, article_ids]) => {return article_ids.includes(article_id)}).map(([category_id, article_ids]) => {return store.category_cache[+category_id]}))
+	let article_stack = $derived(store.article_history.concat(store.undone_articles))
+	let stack_ix = $derived(article_stack.indexOf(article_id))
 
-	// let article_stack = $derived(Array.from(new Set(store.article_history.concat(store.undone_articles).concat(store.article_modal_articles))))
-	// let stack_ix = $derived(article_stack.indexOf(article_id))
-
-	// function set_article_id_from_stack(stack_change: number){
-	// 	if (stack_ix === -1){return}
-	// 	const new_ix = stack_ix + stack_change
-	// 	if (new_ix < 0 || new_ix >= article_stack.length){return}
-	// 	const current_tab = store.article_modal_articles.indexOf(article_id)
-	// 	article_id = article_stack[new_ix]
-	// 	store.article_modal_articles[current_tab] = article_id
-	// 	on_display = 'front-page'
-	// }
+	function set_article_id_from_stack(stack_change: number){
+		if (stack_ix === -1){return}
+		const new_ix = stack_ix + stack_change
+		if (new_ix < 0 || new_ix >= article_stack.length){return}
+		const current_tab = store.article_modal_articles.indexOf(article_id)
+		article_id = article_stack[new_ix]
+		store.article_modal_articles[current_tab] = article_id
+		on_display = 'front-page'
+	}
 
 	function close_article(id: number){
 		store.article_modal_articles = store.article_modal_articles.filter(val => {return id !== val})
@@ -49,37 +41,31 @@
 		if(!store.article_modal_articles.includes(article_id)){store.article_modal_articles.push(article_id)}
 	})
 
-	async function save_content(){
-		if(edit_mode){
-			await dtb.update_article({...article, content: editor?.get_content() ?? article.content, title: article_title?.innerText ?? article.title})
-		}
-	}
+	function updateTitle() {
+		const title: string = article_title.innerText;
 
-	function on_editor_change() {
-		if(editor){latest_editor_content = editor.get_content()}
+		article.title = title;
+        dtb.update_article(article)
 	}
 	
 	function open_article_options(){
 		push_modal(get_article_options(article.id))
 	}
 	
-	let image_source = $derived<string>((article.image && store.image_public_urls[article.image]) ? URL.createObjectURL(store.image_public_urls[article.image]) : '')
+	let image_source = $state('');
+	$effect(() => {
+		if (article.image && store.image_public_urls[article.image]) {
+			image_source = URL.createObjectURL(store.image_public_urls[article.image]);
+		}
+	});
 
 	function change_text_size(factor: number) {
 		text_size = text_size * factor;
 	}
 
-	function set_article_id(id: number) {
-		if(edit_mode){
-			save_content()
-		}
-		article_id = id
-	}
-
 	async function add_tab(){
-		await dtb.fetch_all_from_project(store.project_id)
 		let value: {id: number | null, title: string} = {id: null, title: ''}
-		await push_promise_modal({type: 'choose_modal', data: {Articles: [add_and_choose_article(value)].concat(choose_article_by_id(value))}, use_search: true})
+		await push_promise_modal({type: 'choose_modal', data: {Articles: choose_article_by_id(value)}, use_search: true})
 		if (value.id === null){return}
 		if(!store.article_modal_articles.includes(value.id)){
 			store.article_modal_articles.push(value.id)
@@ -95,7 +81,7 @@
 >
 	<div id='button_bar'>
 
-		<!-- <button
+		<button
 			id="undo_article_button"
 			onclick={() => {
 				set_article_id_from_stack(-1);
@@ -115,32 +101,18 @@
 			style="background-image: url('/assets/arrow_right.png');"
 			title="Go to next Article"
 			aria-label='Redo Button'
-		></button> -->
+		></button>
 
 		<div id="articles_tab">
 			{#each store.article_modal_articles as id}
-				<button class='tab_button' class:disabled={article_id === id} onclick={()=>{set_article_id(id)}}>
-					<p class='tab_text'>{store.article_cache[id]?.title ?? "Unknown"}</p>
+				<button class='tab_button' class:disabled={article_id === id} onclick={()=>{article_id = id}}>
+					<p class='tab_text'>{store.article_cache[id].title}</p>
 					<span class="close" onclick={(e: Event) => {close_article(id); e.stopPropagation()}}>&times;</span>
 				</button>
 			{/each}
 			<button id='add_tab_button' onclick={add_tab}>+
 			</button>
 		</div>
-
-		{#if store.write_access}
-		<button
-			id="edit_article_button"
-			onclick={async () => {
-				if(edit_mode){await save_content()}
-				edit_mode = !edit_mode;
-			}}
-			class:pressed={edit_mode}
-			style="background-image: url('/assets/quill.png');"
-			title="Toggle Edit Mode"
-			aria-label='Toggle Edit Mode'
-		></button>
-		{/if}
 
 		<button
 			id="increment_text_size_button"
@@ -182,72 +154,47 @@
 		</button>
 	</div>
 	{#if on_display === 'front-page'}
-	<div id='front-page'>	
-		<div id="cover">
-			<div
-			id="article_title"
-			contenteditable={edit_mode}
-			class={edit_mode ? 'editable' : 'non-editable'}
-			onblur={() => {
-				save_content();
-			}}
-				>
-				<h1 bind:this={article_title}>{article?.title ?? "Unknown"}</h1>
-			</div>
-			<div id="image_container" style="height: {30}%;">
-				<img
-				id="article_image"
-				src={image_source}
-				alt="Article image"
-				class:hidden={article.image === null}
-				/>
-			</div>
+		<div
+		id="article_title"
+		contenteditable={store.edit_mode}
+		class={store.edit_mode ? 'editable' : 'non-editable'}
+		onblur={() => {
+			updateTitle();
+		}}
+			>
+			<h1 bind:this={article_title}>{article.title}</h1>
 		</div>
-		<ul id="category_list">
-			{#each current_categories as category}
-			<li class="category_entity" style='background-image: url("{theme_entities[category.theme_id].image}");'>
-				<p class='category_name'>{category.name}</p>
-				{#if edit_mode}
-					<button class='option_button' onclick={()=>{push_modal(get_composite_category_modal({...category}))}}></button>
-					<span class="remove_link" onclick={()=>{dtb.delete_article_category_link(store.project_id, category.id, article_id)}}>&times;</span>
-				{/if}
-			</li>
-			{/each}
-			{#if edit_mode}
-			<li class="category_entity" style='background-image: url("{theme_entities[0].image}");' onclick={async ()=>{await get_filtered_choose_category_modal(article_id, current_categories)}}>
-				<p>+ Add category</p>
-			</li>
-			{/if}
-		</ul>
-	</div>
+		<div id="image_container" style="height: {article.image !== null ? 30 : store.edit_mode ? 10 : 0}%;">
+			<img
+			id="article_image"
+			src={image_source}
+			alt="Article image"
+			class:hidden={article.image === null}
+			/>
+		</div>
 	{:else if on_display === 'main'}
 		<div id='article_reader'>
 			<div
 				id="article_content"
+				class={store.edit_mode ? 'editable' : 'non-editable'}
 				style="font-size: {text_size}%;"
 			>
-				<SvelteMarkdown source={editor ? latest_editor_content : article.content} renderers={{link: KeyWordRenderer}}/>
+				{#if store.edit_mode}
+					<Editor />
+				{:else}
+					<SvelteMarkdown source={article.content} renderers={{link: KeyWordRenderer}}/>
+				{/if}
 			</div>
-			{#if edit_mode}
-			<div id="article_editor">
-				<Editor bind:this={editor} on_destroy={save_content} original_content={article.content} text_size={text_size} on_change={on_editor_change}/>
-			</div>
-			{/if}
 		</div>
 	{/if}
 	</div>
 </div>
 
 <style>
-
-	/* Modal */
-
     #article_modal{
         display: flex;
         flex-direction: column;
     }
-
-	/* Tab bar */
 
     #button_bar{
         position: relative;
@@ -297,80 +244,8 @@
 		border: none;
 		box-shadow: none;
 	}
-
-	/* Front page */
 	
-    #front-page{
-		display: flex;
-		gap: 20px;
-		padding: 0px 20px;
-	}
-
-	#cover{
-		display: flex;
-		flex-direction: column;
-		width: 65%;
-		height: 55vh;
-	}
-
-	#category_list{
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		width: 35%;
-		gap: 10px;
-		padding: 20px 0px;
-		
-		background-color: rgb(130, 130, 130);
-		border-radius: 20px;
-		box-shadow: inset 5px 5px 5px rgb(40, 40, 40);
-	}
-
-	.category_entity{
-		position: relative;
-        display: flex;
-        width: 60%;
-        padding: 0 30px;
-        align-items: center;
-        background-color: rgb(70, 70, 70);
-        background-position: top right;
-        background-size: 100%;
-        
-		font-size: x-large;
-		color: var(--main_white);
-        border-radius: 10px;
-        box-shadow: 3px 3px 3px rgb(40, 40, 40);
-	}
-
-	.category_name {
-		flex: 1;
-	}
-
-	.remove_link {
-		cursor: pointer;
-	}
-
-	.remove_link:hover{
-		opacity: 0.7
-	}
-
-	.option_button {
-        position: relative;
-        width: 30px;
-        aspect-ratio: 1;
-		background-color: transparent;
-		background-image: url('/assets/more_vert.png');
-		background-size: contain;
-        background-position: center center;
-		background-repeat: no-repeat;
-		border: none;
-		cursor: pointer;
-	}
-
-    .option_button:hover{
-		opacity: 0.7;
-	}
-
+    
     #article_title {
 		position: relative;
         height: 20%;
@@ -397,18 +272,17 @@
         width: 60%;
         border-radius: 10px;
     }
-
-	/* Main content */
     
 	#article_reader {
-		height: 55vh;
+		max-height: 55vh;
 		
 		background-color: rgb(47, 47, 47);
 		display: flex;
+		flex-direction: column;
 		align-items: center;
-		overflow-y: hidden;
+		overflow-y: auto;
 		gap: 20px;
-		padding: 0px 0px;
+		padding: 30px 0px;
 		border-radius: 20px;
 		
 		box-shadow: inset 5px 0px 5px rgb(20, 20, 20);
@@ -416,11 +290,8 @@
 	
     #article_content {
 		position: relative;
-		max-height: 50vh;
         height: 90%;
-		overflow-y: auto;
-
-        flex: 1;
+        width: 90%;
         font-family: 'Garamond Regular';
         text-align: justify;
         padding: 0px 10px;
@@ -428,12 +299,6 @@
         color: var(--main_white);
         white-space: normal;
     }
-
-	#article_editor{
-		position: relative;
-		height: 90%;
-		flex: 1;
-	}
 	
 	#button_bar button{
 		position: relative;
@@ -497,10 +362,6 @@
 		text-overflow: ellipsis;
 		overflow: hidden;
 	}
-	
-	#edit_article_button.pressed{
-		background-color: rgb(40, 40, 40);
-	}
 
 	.close{
 		position: relative;
@@ -540,20 +401,20 @@
 		padding: 10px 0px 0px 5px;
 	}
 
-	#article_content::-webkit-scrollbar{
+	#article_reader::-webkit-scrollbar{
 		width: 12px;
 	}
 
-	#article_content::-webkit-scrollbar-thumb {
+	#article_reader::-webkit-scrollbar-thumb {
 		background-color: #555;
 		border-radius: 5px;
 	}
 
-	#article_content::-webkit-scrollbar-corner {
+	#article_reader::-webkit-scrollbar-corner {
 		background-color: rgb(47, 47, 47);
 	}
 
-	#article_content::-webkit-scrollbar-thumb:hover {
+	#article_reader::-webkit-scrollbar-thumb:hover {
 		background-color: #888;
 	}
 
